c38f1ae6faacadc9de4e344094f2aa2ca4754e87
mariohectem <53344437+mariohectem@users.noreply.github.com>
2021-01-25 13:57:06 -0700

    Update crearArchivo.php

--- crearArchivo.php
+++ crearArchivo.php
@@ -4,7 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 header("Content-Type: application/json; charset=UTF-8");
-$cadbts="<html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><script src=js/jquery.min.js></script><script src='js/bootstrap.min.js'></script><link href='css/bootstrap.min.css' rel='stylesheet'></head>";
+$cadbts="<html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+ if($_POST["fbobs"]=="bs") 
+  $cadbts.= "<script src=js/jquery.min.js></script><script src='js/bootstrap.min.js'></script><link href='css/bootstrap.min.css' rel='stylesheet'>";
+$cadbts.="</head>";
 $text=$cadbts;
 $text.="<body>";
 $text.=$_POST["val"];
@@ -14,10 +17,11 @@ $nomarch=$_POST["nomarch"];
 $text.="</body>";
 $text .="<script>var op=JSON.parse('".$op."'); var cb=JSON.parse('".$cb."');</script>";//document.getElementById('mio').value);</script>";
 $text .="<script src='catfp.js'></script>";
-$text .= "<script>cargarSelectsNoDependientes();crearDivBusqueda();</script>";
+$text .= "<script>cargarSelectsNoDependientes();crearDivBusqueda();oeid('nomarch').style.display='none';oeid('btncarch').style.display='none';</script>";
  $bol=unlink($nomarch);
  $fh=fopen($_SERVER['DOCUMENT_ROOT'].'/tv/'.$nomarch,'a+');
  // $stringData = "your html code php code goes here";   
   fwrite($fh, $text);
   fclose($fh);
 ?>
+
